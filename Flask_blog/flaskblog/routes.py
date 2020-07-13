261f5183bb3cfe3280c571f26c4dccaf013062b8
Daenamu <you@dheodnr318@naver.com>
2020-07-14 02:52:35 +0900

    restructure tree
    모듈화: __init__ 파일 만들기.
    
    run.py 실행->
    flaskblog 모듈의 __init__파일 실행.
    
    변수 생성 ->
    routes 파일 import(실행)
    
    from flaskblog import app 이나
    from flaskblog import db는
    __init__을 실행하는데, __main__을 썼던 것과 같은 효과가 발생함. 따라서 변수 전달 가능.

--- Flask_blog/flaskblog/routes.py
+++ Flask_blog/flaskblog/routes.py
@@ -1,13 +1,7 @@
-from flask import Flask, render_template, url_for, flash, redirect
-from forms import RegistrationForm, LoginForm
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '2182ff5d631949b09960ff03b5f1e2c2'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-
-from models import User, Post
+from flask import render_template, url_for, flash, redirect
+from flaskblog import app
+from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog.models import User, Post
 
 posts = [
     {
@@ -55,7 +49,3 @@ def login():
         else:
             flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
