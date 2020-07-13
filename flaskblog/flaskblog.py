28c10b93b7f7577f3aac61f5dec4e6cf8c6e6a1a
Daenamu <you@dheodnr318@naver.com>
2020-07-14 00:16:03 +0900

    make user input forms
    registration form

--- flaskblog/flaskblog.py
+++ flaskblog/flaskblog.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '2182ff5d631949b09960ff03b5f1e2c2'
 
 posts = [
     {
@@ -17,6 +20,7 @@ posts = [
     }
 ]
 
+
 @app.route('/')
 @app.route('/home')
 def hello():
@@ -26,6 +30,18 @@ def hello():
 @app.route('/about')
 def about():
     return render_template("about.html", title="about")
+
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
