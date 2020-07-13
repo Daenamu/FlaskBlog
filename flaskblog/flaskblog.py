bffba652855b02a80c7adc8ef2bd29b73b6c1bad
Daenamu <you@dheodnr318@naver.com>
2020-07-14 02:05:27 +0900

    database package structure
    from __main__ import db
    를 통해 원래 모듈에서 import한 순간에 정보를 가져올 수 있음.
    
    from flaskblog import db
    이 때는 변수를 import 할 수 없음

--- flaskblog/flaskblog.py
+++ flaskblog/flaskblog.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 from flask_sqlalchemy import SQLAlchemy
@@ -8,35 +7,7 @@ app.config['SECRET_KEY'] = '2182ff5d631949b09960ff03b5f1e2c2'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
-
-class User(db.Model):
-    __table_name__ = 'user'
-
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
-    password = db.Column(db.String(60), nullable=False)
-
-    posts = db.relationship('Post', backref='author', lazy=True)  # relationship 은  객체 이름을 받음.
-
-    def __repr__(self):
-        return f"User('{self.username}','{self.email}','{self.image_file}')"
-
-
-class Post(db.Model):
-    __table_name__ = 'post'
-
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    content = db.Column(db.Text, nullable=False)
-
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)  # foreignkey 는 테이블 이름을 인자로 받음.
-
-    def __repr__(self):
-        return f"Post('{self.title}','{self.date_posted}')"
-
+from models import User, Post
 
 posts = [
     {
