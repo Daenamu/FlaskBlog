e35b7e27608678839cea7f47d05ca8a06a59b6f2
Daenamu <you@dheodnr318@naver.com>
2020-07-14 16:45:23 +0900

    User register - store in db
    사용자 정의 함수를 통해 직접 validate 가능

--- Flask_blog/flaskblog/__init__.py
+++ Flask_blog/flaskblog/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '2182ff5d631949b09960ff03b5f1e2c2'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+db.create_all()
+bcrypt = Bcrypt(app)
 
 from flaskblog import routes
