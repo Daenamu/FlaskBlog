43f8d3c63273263f832e1e12ef2d3a1daba95682
Daenamu <you@dheodnr318@naver.com>
2020-07-14 17:25:41 +0900

    User account page and access

--- Flask_blog/flaskblog/__init__.py
+++ Flask_blog/flaskblog/__init__.py
@@ -10,5 +10,8 @@ db = SQLAlchemy(app)
 db.create_all()
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
 
 from flaskblog import routes
