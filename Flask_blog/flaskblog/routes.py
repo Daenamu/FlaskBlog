570dcc44c30a545aa44632c186d7d9291346eb3e
Daenamu <you@dheodnr318@naver.com>
2020-07-14 20:23:11 +0900

    resize image file and delete old file

--- Flask_blog/flaskblog/routes.py
+++ Flask_blog/flaskblog/routes.py
@@ -1,5 +1,6 @@
 import os
 import secrets
+from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
@@ -75,7 +76,12 @@ def save_picture(form_picture):
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
     picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
-    form_picture.save(picture_path)
+
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+
+    i.save(picture_path)
     return picture_fn
 
 
@@ -85,6 +91,8 @@ def account():
     form = UpdateAccountForm()
     if form.validate_on_submit():
         if form.picture.data:
+            file_path = os.path.join(app.root_path, 'static/profile_pics', current_user.image_file)
+            os.remove(file_path)
             picture_file = save_picture(form.picture.data)
             current_user.image_file = picture_file
         current_user.username = form.username.data
