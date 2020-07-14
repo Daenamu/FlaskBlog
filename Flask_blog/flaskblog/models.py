707264620326e0563049a5d0ac35b86be23d3486
Daenamu <you@dheodnr318@naver.com>
2020-07-14 17:11:48 +0900

    User login

--- Flask_blog/flaskblog/models.py
+++ Flask_blog/flaskblog/models.py
@@ -1,8 +1,14 @@
-from flaskblog import db
+from flaskblog import db, login_manager
 from datetime import datetime
+from flask_login import UserMixin
 
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
     __table_name__ = 'user'
 
     id = db.Column(db.Integer, primary_key=True)
