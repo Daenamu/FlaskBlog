b9fcf8a3faa8946ffef16a09b66e47ef7dc5db4b
Daenamu <you@dheodnr318@naver.com>
2020-07-15 21:16:32 +0900

    custom error pages

--- Flask_blog/flaskblog/__init__.py
+++ Flask_blog/flaskblog/__init__.py
@@ -16,7 +16,7 @@ login_manager.login_message_category = 'info'
 
 def create_app(config_class=Config):
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config.from_object(config_class)
 
     mail.init_app(app)
     db.init_app(app)
@@ -26,9 +26,11 @@ def create_app(config_class=Config):
     from flaskblog.users.routes import users
     from flaskblog.posts.routes import posts
     from flaskblog.main.routes import main
+    from flaskblog.errors.handlers import errors
 
     app.register_blueprint(users)
     app.register_blueprint(posts)
     app.register_blueprint(main)
+    app.register_blueprint(errors)
 
     return app
