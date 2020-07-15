e10027217fbb17cb18b6b918c60556a7aa78735f
Daenamu <you@dheodnr318@naver.com>
2020-07-15 20:20:05 +0900

    module
    app을 생성하는 init파일을 객체화 시킴.

--- Flask_blog/flaskblog/__init__.py
+++ Flask_blog/flaskblog/__init__.py
@@ -5,25 +5,30 @@ from flask_login import LoginManager
 from flask_mail import Mail
 from flaskblog.config import Config
 
-app = Flask(__name__)
-app.config.from_object(Config)
 
-mail = Mail(app)
-
-db = SQLAlchemy(app)
-db.create_all()
-
-bcrypt = Bcrypt(app)
-
-login_manager = LoginManager(app)
+mail = Mail()
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 
 
-from flaskblog.users.routes import users
-from flaskblog.posts.routes import posts
-from flaskblog.main.routes import main
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
 
-app.register_blueprint(users)
-app.register_blueprint(posts)
-app.register_blueprint(main)
+    mail.init_app(app)
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+
+    from flaskblog.users.routes import users
+    from flaskblog.posts.routes import posts
+    from flaskblog.main.routes import main
+
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+
+    return app
